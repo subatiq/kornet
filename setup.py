212ef15042504baec01c6874046962faa74b04f2
Vladimir Semenov <sepsseven@gmail.com>
2022-05-14 15:38:52 +0300

    add installation files

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'chieftane',
   packages = ['chieftane'],
-  version = 'pre-0.0.0',
+  version = '0.0.0',
   license='MIT',
   description = 'A library for mass execution of ssh commands on remote machines fleet.',
   author = 'Vladimir Semenov',
   author_email = 'subatiq@gmail.com',
   url = 'https://github.com/subatiq/chieftane',
-  download_url = 'https://github.com/subatiq/chieftane/archive/refs/tags/pre-0.0.0.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/subatiq/chieftane/archive/refs/tags/pre-0.0.0.tar.gz',
   keywords = ['SSH', 'AUTOMATION', 'ORCHESTRATION'],
   install_requires=[
         'paramiko',
@@ -18,10 +18,11 @@ setup(
         'typer',
       ],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
-    'Topic :: Automation',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3.8',
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: System Administrators',
+    'Topic :: System :: Systems Administration',
+    'Topic :: Utilities',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.9',
   ],
 )
